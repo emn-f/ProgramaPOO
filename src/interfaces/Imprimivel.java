c20784e9c0c89291db3d67873f1bf26b9c9c3d7a
emanuel <95362058+emn-f@users.noreply.github.com>
2025-06-04 11:49:01 -0300

    Classe OK

--- src/interfaces/Imprimivel.java
+++ src/interfaces/Imprimivel.java
@@ -1,5 +1,9 @@
 package interfaces;
 
+/**
+ * Interface que define um contrato para classes que podem imprimir um resumo de seus dados.
+ * Qualquer classe que implementar esta interface deve fornecer uma implementação para o método imprimirResumo().
+ */
 public interface Imprimivel {
     void imprimirResumo();
 }
